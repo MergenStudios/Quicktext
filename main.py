66e1cc53546ee896fba4a9313f92abaeb463473c
MergenStudios <paulthorand.2005@gmail.com>
2022-03-27 12:45:54 +1030

    cleaned up code

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import sys, threading, time, pyautogui
-from turtle import window_height
+import sys
 
-from PySide6.QtWidgets import QMainWindow, QApplication, QFrame
+from PySide6.QtWidgets import QMainWindow, QApplication
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import Qt
-from PySide6 import QtCore
 
 from qt_material import apply_stylesheet
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
 
         loader = QUiLoader()
         self.ui = loader.load("main_ui.ui", None)
@@ -23,7 +19,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     
-    # window.setWindowFlag(Qt.FramelessWindowHint)
     apply_stylesheet(app, "dark_teal.xml")
     window.setGeometry(100, 100, 229, 41)
 
