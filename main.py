fbf83831bccda1ce2bf0104142e1a222e6381825
MergenStudios <paulthorand.2005@gmail.com>
2022-03-29 08:59:22 +1030

    added dragging support

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
+import sys, pyautogui
 
 from PySide6.QtWidgets import QMainWindow, QApplication
 from PySide6.QtUiTools import QUiLoader
+from PySide6.QtGui import Qt
 
 from qt_material import apply_stylesheet
 
@@ -9,18 +10,33 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowFlag(Qt.FramelessWindowHint)
+
         loader = QUiLoader()
         self.ui = loader.load("main_ui.ui", None)
 
+
         self.setCentralWidget(self.ui)
 
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPosition().toPoint()
+
+
+    def mouseMoveEvent(self, event):
+      self.move(self.pos() + event.globalPosition().toPoint() - self.dragPos )
+      self.dragPos = event.globalPosition().toPoint()
+      event.accept()
+
+    def mouseDoubleClickEvent(self, event):
+        self.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     
     apply_stylesheet(app, "dark_teal.xml")
-    window.setGeometry(100, 100, 229, 41)
+    window.setFixedWidth(250)
+    window.setFixedHeight(55)
 
     window.show()
 
